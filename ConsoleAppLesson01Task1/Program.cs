047203308fbd2a97fdbb5993f81d2dd866cc1293
sagyndykovaaida <lvnidass@gmail.com>
2023-09-21 17:51:02 +0600

    commit: modul 1

---
+++ ConsoleAppLesson01Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string first_name = Console.ReadLine();
+            Console.Write("Введите свою фамилию: ");
+            string second_name = Console.ReadLine();
+            Console.Write($"Приветствую тебя {second_name} {first_name}");
+            Console.ReadKey();
+        }
+    }
+}
